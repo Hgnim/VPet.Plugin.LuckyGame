fa0c8b8d5039d1c55d8447e5fe1a100acd8d54f7
EvianChu <146960797+YIYuNCU@users.noreply.github.com>
2025-11-24 16:01:57 +0800

    完成部分兑换页面

--- VPet.Plugin.LuckyGame/LuckyGame.cs
+++ VPet.Plugin.LuckyGame/LuckyGame.cs
@@ -1,11 +1,104 @@
-﻿using VPet_Simulator.Windows.Interface;
+﻿using LinePutScript.Localization.WPF;
+using Panuon.WPF;
+using Panuon.WPF.UI;
+using System;
+using System.Windows.Controls;
+using VPet.Plugin.LuckyGame.Windows;
+using VPet_Simulator.Windows.Interface;
 
 namespace VPet.Plugin.LuckyGame
 {
     public class LuckyGame : MainPlugin {
-		public LuckyGame(IMainWindow mainwin) : base(mainwin) {
+        public Fortune fortuneWindow;
+        public ArcadeExchange arcadeExchangeWindow;
+        public LuckyGame(IMainWindow mainwin) : base(mainwin) {
 		}
 		public override string PluginName => "LuckyGame";
+        public override void LoadPlugin()
+        {
+            
+        }
 
+        public override void GameLoaded()
+        {
+            var TokenExchangeMenu = new MenuItem
+            {
+                Header = "TokenExchangge".Translate(),
+                HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+            };
+            TokenExchangeMenu.Click += TokenExchangeMenu_Click;
+            MW.Main.ToolBar.MenuFeed.Items.Add(TokenExchangeMenu);
+        }
+
+        private void TokenExchangeMenu_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if(arcadeExchangeWindow != null && arcadeExchangeWindow.IsVisible)
+            {
+                arcadeExchangeWindow.Activate();
+                return;
+            }
+            else if(arcadeExchangeWindow != null && !arcadeExchangeWindow.IsVisible)
+            {
+                arcadeExchangeWindow.Close();
+                arcadeExchangeWindow = null;
+                arcadeExchangeWindow = new ArcadeExchange(MW);
+                arcadeExchangeWindow.Show();
+            }
+            else
+            {
+                arcadeExchangeWindow = new ArcadeExchange(MW);
+                arcadeExchangeWindow.Show();
+            }
+        }
+
+        private void Fortune_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if(fortuneWindow != null && fortuneWindow.IsVisible)
+            {
+                fortuneWindow.Activate();
+                return;
+            }
+            else if(fortuneWindow != null && !fortuneWindow.IsVisible)
+            {
+                fortuneWindow.Close();
+                fortuneWindow = null;
+                fortuneWindow = new Fortune();
+                fortuneWindow.Show();
+            }
+            else
+            {
+                fortuneWindow = new Fortune();
+                fortuneWindow.Show();
+            }
+        }
+
+        public override void Setting()
+        {
+            Fortune_Click(this, null);
+        }
+
+        public override void LoadDIY()
+        {
+            try
+            {
+                var menu = new MenuItem
+                {
+                    Header = "Lucky Game".Translate(),
+                    HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+                };
+                var fortune = new MenuItem
+                {
+                    Header = "Fortune".Translate(),
+                    HorizontalAlignment = System.Windows.HorizontalAlignment.Center
+                };
+                fortune.Click += Fortune_Click;
+                menu.Items.Add(fortune);
+                MW.Main.ToolBar.MenuDIY.Items.Add(menu);
+            }
+            catch (Exception ex)
+            {
+                MessageBoxX.Show("DIY列表加载错误{0}".Translate(ex.Message), "错误".Translate());
+            }
+        }
     }
 }
---
+++ VPet.Plugin.LuckyGame/Windows/ArcadeExchange.xaml.cs
@@ -0,0 +1,114 @@
+﻿using LinePutScript.Localization.WPF;
+using Panuon.WPF.UI;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using VPet.Plugin.LuckyGame.Core;
+using VPet_Simulator.Windows.Interface;
+
+namespace VPet.Plugin.LuckyGame.Windows
+{
+    public partial class ArcadeExchange : Window
+    {
+        private double cashBalance = 0.0;
+        private int gameCoins = 2500;
+        private GameTokenCoin TokenCoin;
+        private IMainWindow MainWindow;
+        public ArcadeExchange(IMainWindow mainWindow)
+        {
+            MainWindow = mainWindow;
+            InitializeComponent();
+            InitializeArcade();
+        }
+
+        private void InitializeArcade()
+        {
+            TokenCoin = new();
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            CashBalanceText.Text = $"¥ {cashBalance:N2}";
+            CoinBalanceText.Text = $"🎮 {gameCoins:N0} 枚";
+        }
+
+        private void ExchangeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && double.TryParse(button.Tag.ToString(), out double amount))
+            {
+                ProcessExchange(amount);
+            }
+        }
+
+        private void CustomExchangeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(CustomAmountText.Text, out double amount) && amount > 0)
+            {
+                ProcessExchange(amount);
+            }
+            else
+            {
+                ShowMessage("请输入有效的兑换金额", "warn");
+            }
+        }
+
+        private void ProcessExchange(double amount)
+        {
+            var result = TokenCoin.ChangeCoin(MainWindow, GameTokenCoin.CoinType.coinBlack, (long)amount);
+            switch(result)
+            {
+                case 0:
+                    UpdateDisplay();
+                    ShowMessage($"兑换成功！");
+                    break;
+                case 1:
+                    ShowMessage("兑换失败，发生未知错误","error");
+                    break;
+                case 2:
+                    ShowMessage("兑换失败，桌宠钱余额不足","warn");
+                    break;
+                case 3:
+                    ShowMessage("兑换失败，游戏币余额不足","warn");
+                    break;
+                case 4:
+                    ShowMessage("兑换失败，兑换金额必须非零","warn");
+                    break;
+            }
+        }
+
+        private int GetPackageBonus(double amount)
+        {
+            return amount switch
+            {
+                10 => 10,
+                20 => 20,
+                50 => 100,
+                100 => 300,
+                200 => 800,
+                _ => (int)(amount * 1) // 自定义兑换无额外奖励或基础奖励
+            };
+        }
+
+        private void ShowMessage(string message,string title = "info")
+        {
+            MessageBoxX.Show(message.Translate(),title.Translate());
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+        }
+
+        private void GameButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
