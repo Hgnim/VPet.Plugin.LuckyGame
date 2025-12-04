5556cc8c9b8d8decd331599a7f515a4891c160c8
Hgnim <hgn1m@outlook.com>
2025-12-04 22:42:03 +0800

    Lottery奖金算法修复

--- VPet.Plugin.LuckyGame/Core/Game/Lottery.cs
+++ VPet.Plugin.LuckyGame/Core/Game/Lottery.cs
@@ -273,12 +273,12 @@ namespace VPet.Plugin.LuckyGame.Core.Game {
 				ulong[] winCoDet = new ulong[8];
 				{
 					double[] winCoDet_doub = new double[8];
+#pragma warning disable IDE0045
 					for (byte b = 0; b < 8; b++) {
 						byte winN = 0;
 						if (b < 6) {
 							for (byte b2 = 0; b2 < b+1; b2++) {
-								if (b2 < 6) 
-									if (mainWinLoc[b2]) winN++;
+								if (mainWinLoc[b2]) winN++;
 							}
 							if (mainWinLoc[b])
 								winCoDet_doub[b] =
@@ -287,18 +287,25 @@ namespace VPet.Plugin.LuckyGame.Core.Game {
 									: 0
 									+
 									item.coin * Math.Pow(8, winN);
+							else
+								winCoDet_doub[b] = 
+									b - 1 >= 0
+									? winCoDet_doub[b - 1]
+									: 0;
 						}
 						else {
 							for(byte b2 = 0; b2 < b-6+1; b2++) {
-								if (b2 < 2)
-									if (deputyWinLoc[b2]) winN++;
+								if (deputyWinLoc[b2]) winN++;
 							}
 							if (deputyWinLoc[b - 6])
 								winCoDet_doub[b] = winCoDet_doub[b - 1] + item.coin * winN;
+							else
+								winCoDet_doub[b] = winCoDet_doub[b - 1];
 							if (b == 8 - 1 && winN == 0) winCoDet_doub[b] = 0;//如果副号码没有赢，则主号码赢得的不算数
 						}
 					}
-					for(byte b = 0; b < 8; b++) {
+#pragma warning restore IDE0045
+					for (byte b = 0; b < 8; b++) {
 						winCoDet[b] = (ulong)Math.Round(winCoDet_doub[b]);
 					}
 				}
